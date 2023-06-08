95d5a2ecb1f1459782489098cad1a66d37f3cbfe
Peter Krautzberger <p.krautzberger@gmail.com>
2023-06-08 21:58:29 +0200

    feat: add error message
    Not sure how to test it but just in case.

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ function doSomethingWithFiles(fileList) {
     // Spinner SVG copyright (c) Utkarsh Verma under MIT License (MIT), https://github.com/n3r4zzurr0/svg-spinners/blob/abfa05c49acf005b8b1e0ef8eb25a67a7057eb20/svg-smil/bars-scale.svg#LL1C1-L1C1
 
     tessOutput.innerHTML = 'Texterkennung arbeitet <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><rect x="1" y="6" width="2.8" height="12"><animate id="spinner_CcmT" begin="0;spinner_IzZB.end-0.1s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="0;spinner_IzZB.end-0.1s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="5.8" y="6" width="2.8" height="12"><animate begin="spinner_CcmT.begin+0.1s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.1s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="10.6" y="6" width="2.8" height="12"><animate begin="spinner_CcmT.begin+0.2s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.2s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="15.4" y="6" width="2.8" height="12"><animate begin="spinner_CcmT.begin+0.3s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.3s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="20.2" y="6" width="2.8" height="12"><animate id="spinner_IzZB" begin="spinner_CcmT.begin+0.4s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.4s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect></svg>';
-    // tesseract (loaded via script)
+
+    // run tesseract (loaded via script)
     // https://github.com/naptha/tesseract.js/blob/master/examples/browser/image-processing.html
     Tesseract.recognize(file, "deu", {
       corePath: './tesseract-core-simd.wasm.js',
@@ -30,7 +31,10 @@ function doSomethingWithFiles(fileList) {
       logger: (m) => console.log(m)
     }).then(
       ({ data: { text } }) => {
-        if (!text) return;
+        if (!text) {
+          tessOutput.innerHTML = '😞 Es tut mir leid, da ist etwas schief gegangen. <br> Bitte versuche es noch einmal oder frage um Hilfe.'
+          return;
+        }
         // console.log(text);
         // clear output
         tessOutput.innerHTML = '';
