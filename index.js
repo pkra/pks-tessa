84585b8905a7eae998dff781bf520cfdd2bb9a72
Peter Krautzberger <p.krautzberger@gmail.com>
2023-06-08 21:57:25 +0200

    feat: add OCR spinner
    * index.js
      * when image is selected, insert spinner + text into output
      * clear output before inserting tesseract result
    
    Resolves #4

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ function doSomethingWithFiles(fileList) {
 
   if (file !== null) {
     output.src = URL.createObjectURL(file);
-    tessOutput.innerHTML = '';
+    
+    // Indicate processing
+    // Spinner SVG copyright (c) Utkarsh Verma under MIT License (MIT), https://github.com/n3r4zzurr0/svg-spinners/blob/abfa05c49acf005b8b1e0ef8eb25a67a7057eb20/svg-smil/bars-scale.svg#LL1C1-L1C1
+
+    tessOutput.innerHTML = 'Texterkennung arbeitet <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><rect x="1" y="6" width="2.8" height="12"><animate id="spinner_CcmT" begin="0;spinner_IzZB.end-0.1s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="0;spinner_IzZB.end-0.1s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="5.8" y="6" width="2.8" height="12"><animate begin="spinner_CcmT.begin+0.1s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.1s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="10.6" y="6" width="2.8" height="12"><animate begin="spinner_CcmT.begin+0.2s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.2s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="15.4" y="6" width="2.8" height="12"><animate begin="spinner_CcmT.begin+0.3s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.3s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="20.2" y="6" width="2.8" height="12"><animate id="spinner_IzZB" begin="spinner_CcmT.begin+0.4s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.4s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect></svg>';
     // tesseract (loaded via script)
     // https://github.com/naptha/tesseract.js/blob/master/examples/browser/image-processing.html
     Tesseract.recognize(file, "deu", {
@@ -28,6 +32,9 @@ function doSomethingWithFiles(fileList) {
       ({ data: { text } }) => {
         if (!text) return;
         // console.log(text);
+        // clear output
+        tessOutput.innerHTML = '';
+        // Add tesseract results
         text.split('\n').forEach(substring => {
           tessOutput.insertAdjacentHTML('beforeend', `<p>${substring}</p>`);
         })
