6ad3df3f3785a1e3be5ccd9b89c315471cbcac1d
Peter Krautzberger <p.krautzberger@gmail.com>
2023-10-03 12:31:24 +0200

    fix: try to fix serviceworker on github pages

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("./service-worker.js");
+  navigator.serviceWorker.register("./service-worker.js", {
+    scope: "./",
+  });
 }
 
 // image input -- via https://web.dev/media-capturing-images/
