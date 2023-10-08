58c84d936da12dfb72abc652d0636e7de3934f37
Peter Krautzberger <p.krautzberger@gmail.com>
2023-10-08 21:13:28 +0200

    feat!: rewrite UI
    * index.html
      * h1 copy & design
      * add wrapper divs for steps
      * step 1: rewrite to single label with visually hidden input
        * focus-within and cursor styles
      * summary/details with image: move  to step 2
      * rewrite output to playback ui as single flexbox
      * move "restart" link to footer
      * redesign footer
    * index.js
      * adjust to wrappers as main separators
      * change UX to hide step 1 completely
      * revise spinner as element
        * adjust text
        *  place after wrapper 1 so it stays visible after hiding step 1
        * remove after OCR
      * move focus to step 2
      * OCR spans: add tabindex
      * adjust selectors
      * manualMove(): use data attribute and move focus
        * note: index.html now has focus styles
    
    Resolves #8

--- index.js
+++ index.js
@@ -22,11 +22,17 @@ function doSomethingWithFiles(fileList) {
 
   if (file !== null) {
     output.src = URL.createObjectURL(file);
-    
+
+    const wrappers = document.querySelectorAll('.wrapper');
+    wrappers[0].setAttribute('hidden', '');
+
     // Indicate processing
     // Spinner SVG copyright (c) Utkarsh Verma under MIT License (MIT), https://github.com/n3r4zzurr0/svg-spinners/blob/abfa05c49acf005b8b1e0ef8eb25a67a7057eb20/svg-smil/bars-scale.svg#LL1C1-L1C1
 
-    tessOutput.innerHTML = 'Texterkennung arbeitet <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><rect x="1" y="6" width="2.8" height="12"><animate id="spinner_CcmT" begin="0;spinner_IzZB.end-0.1s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="0;spinner_IzZB.end-0.1s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="5.8" y="6" width="2.8" height="12"><animate begin="spinner_CcmT.begin+0.1s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.1s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="10.6" y="6" width="2.8" height="12"><animate begin="spinner_CcmT.begin+0.2s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.2s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="15.4" y="6" width="2.8" height="12"><animate begin="spinner_CcmT.begin+0.3s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.3s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="20.2" y="6" width="2.8" height="12"><animate id="spinner_IzZB" begin="spinner_CcmT.begin+0.4s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.4s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect></svg>';
+    const spinner = document.createElement('div');
+    spinner.classList.add('wrapper');
+    spinner.innerHTML = 'Bitte warten <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><rect x="1" y="6" width="2.8" height="12"><animate id="spinner_CcmT" begin="0;spinner_IzZB.end-0.1s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="0;spinner_IzZB.end-0.1s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="5.8" y="6" width="2.8" height="12"><animate begin="spinner_CcmT.begin+0.1s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.1s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="10.6" y="6" width="2.8" height="12"><animate begin="spinner_CcmT.begin+0.2s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.2s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="15.4" y="6" width="2.8" height="12"><animate begin="spinner_CcmT.begin+0.3s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.3s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect><rect x="20.2" y="6" width="2.8" height="12"><animate id="spinner_IzZB" begin="spinner_CcmT.begin+0.4s" attributeName="y" calcMode="spline" dur="0.6s" values="6;1;6" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/><animate begin="spinner_CcmT.begin+0.4s" attributeName="height" calcMode="spline" dur="0.6s" values="12;22;12" keySplines=".36,.61,.3,.98;.36,.61,.3,.98"/></rect></svg>'
+    wrappers[0].insertAdjacentElement('afterend', spinner);
 
     // run tesseract (loaded via script)
     // https://github.com/naptha/tesseract.js/blob/master/examples/browser/image-processing.html
@@ -36,6 +42,7 @@ function doSomethingWithFiles(fileList) {
       logger: (m) => console.log(m)
     }).then(
       ({ data: { text } }) => {
+        spinner.remove()
         if (!text) {
           tessOutput.innerHTML = '😞 Es tut mir leid, da ist etwas schief gegangen. <br> Bitte versuche es noch einmal oder frage um Hilfe.'
           return;
@@ -43,6 +50,7 @@ function doSomethingWithFiles(fileList) {
         // console.log(text);
         // toggle instruction step highlight to Step 2
         document.querySelectorAll('.instruction').forEach(node => node.classList.toggle('instruction--active'));
+        step2.focus();
 
         // clear output
         tessOutput.innerHTML = '';
@@ -54,12 +62,13 @@ function doSomethingWithFiles(fileList) {
         tessOutput.querySelectorAll('p').forEach(p => {
           const substring = p.innerHTML;
           p.innerHTML = '';
-          substring.split(' ').forEach(word => { if (word) p.insertAdjacentHTML('beforeend', `<span>${word}</span> `) })
+          substring.split(' ').forEach(word => { if (word) p.insertAdjacentHTML('beforeend', `<span tabindex="0">${word}</span> `) })
         })
 
         // enable UI
         document.querySelectorAll('[disabled]').forEach(node => node.removeAttribute('disabled'))
-        document.querySelectorAll('[hidden]').forEach(node => node.removeAttribute('hidden'));
+        wrappers[1].removeAttribute('hidden');
+        document.querySelector('details[hidden]').removeAttribute('hidden');
       }
     );
   }
@@ -73,15 +82,16 @@ let controller;
 const manualMove = (forward) => {
   const increment = forward ? +1 : -1
   const spans = [...tessOutput.querySelectorAll('span')];
-  const currentIndex = spans.findIndex(node => node.hasAttribute('style'));
+  const currentIndex = spans.findIndex(node => node.hasAttribute('data-currentWord'));
   if (forward && (currentIndex === spans.length - 1)) {
     if (play.checked) play.click()
     return;
   }
   const next = spans[currentIndex + increment] || spans[0];
   // console.log(next)
-  next.style.backgroundColor = 'yellow';
-  spans[currentIndex]?.removeAttribute('style');
+  next.setAttribute('data-currentWord', '');
+  next.focus();
+  spans[currentIndex]?.removeAttribute('data-currentWord');
   window.speechSynthesis.cancel();
   ssu.text = next.textContent;
   window.speechSynthesis.speak(ssu);
